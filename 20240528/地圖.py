1246bf8783d002abf11397231524c3d9868ccd42
11272007 <g11272007@cycu.edu.tw>
2024-05-23 16:54:10 +0800

    1

--- 20240528/地圖.py
+++ 20240528/地圖.py
@@ -3,19 +3,22 @@ import glob
 
 # 讀取csv檔案
 df1 = pd.read_csv('ETag.csv')
-df2 = pd.read_csv('高速公路資訊\\20240101.csv')
+df2 = pd.read_csv('D:\\download\\TDCS_M05A_20240522_235500.csv')
 
-# 比較df1中方向為0的里程和df2中方向為0且時間為1的里程
-df1 = df1[df1['方向'] == 0].copy()
-df2 = df2[(df2['方向'] == 0) & (df2['時間'] == 1)].copy()
+# 保留df1的ETag欄位
+df1 = df1[['ETag']]
 
-# df1和df2只保留里程欄位
-df1 = df1[['里程']].copy()
-df2 = df2[['里程']].copy()
+# 保留df2的第二欄並轉換為DataFrame
+df2 = df2.iloc[:, 1].to_frame()
+# 命名為ETag
+df2.columns = ['ETag']
+# 將重複的資料刪除
+df2 = df2.drop_duplicates()
 
-# 比較df1和df2，列出兩個DataFrame中非重複的資料
-df1 = df1.merge(df2, on='里程', how='outer', indicator=True)
-df1 = df1[df1['_merge'] != 'both'].copy()
+# 將df1與df2重複的資料列出
+df = pd.merge(df1, df2, on='ETag', how='inner')
 
-# 打印出df1和df2有幾筆資料相同
-print(df1)
+# 將結果寫入csv檔案
+df.to_csv('result.csv', index=False, encoding='utf-8-sig')
+
+
